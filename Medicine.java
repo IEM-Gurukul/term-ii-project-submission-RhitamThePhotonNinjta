273ddc408af0b9272b11aa1110dbd8d792667385
RhitamThePhotonNinjta <biswasrhitam834@gmail.com>
2026-03-26 23:21:50 +0530

    medicine file editted

--- Medicine.java
+++ Medicine.java
@@ -1,3 +1,7 @@
 public class Medicine extends Treatment {
- 
+    public void treat(Patient p) {
+        System.out.println("Providing medicines to " + p.getName());
+        p.addHistory("Medicine given");
+    }
+
 }
