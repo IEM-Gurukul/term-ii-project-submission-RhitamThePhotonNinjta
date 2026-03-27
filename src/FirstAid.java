d45bf0cc470f2179f69759338ba84f79d8f6fd77
RhitamThePhotonNinjta <biswasrhitam834@gmail.com>
2026-03-27 08:55:02 +0530

    FirstAid code is added

--- src/FirstAid.java
+++ src/FirstAid.java
@@ -1,3 +1,6 @@
 public class FirstAid {
-    
+       public void treat(Patient p) {
+        System.out.println("Providing first aid to " + p.getName());
+        p.addHistory("First Aid given");
+    }
 }
