bb04c6101908df9d9a7897685fea1af7a06a1c8c
RhitamThePhotonNinjta <biswasrhitam834@gmail.com>
2026-03-29 10:07:27 +0530

    Doctor file is added

---
+++ src/Doctor.java
@@ -0,0 +1,29 @@
+public class Doctor {
+    String docName;
+
+    Doctor(String docName) {
+        this.docName = docName;
+    }
+
+    public void treatPatient(Patient p) {
+
+        System.out.println("\n Doctor " + docName + " is checking " + p.getName());
+
+        Treatment t;
+
+        if (p.getPriority().equalsIgnoreCase("critical")) {
+            System.out.println(" Critical Condition! Sending to Emergency...");
+            t = new Surgery();
+        } else {
+            if (p.getCond().equalsIgnoreCase("fever")) {
+                t = new Medicine();
+            } else if (p.getCond().equalsIgnoreCase("injury")) {
+                t = new FirstAid();
+            } else {
+                t = new Treatment();
+            }
+        }
+
+        t.treat(p);
+    }
+}
