04759e0fce80da526b45204ac217ce9fe9a06ef7
RhitamThePhotonNinjta <biswasrhitam834@gmail.com>
2026-03-29 11:46:08 +0530

    Patient file is added

--- src/Patient.java
+++ src/Patient.java
@@ -1,48 +1,19 @@
-import java.util.*;
-public class Patient implements Runnable {
-private String name;
-private int age;
-private String condition;
-private String priority;
-private int heartRate;
-private String bloodPressure;
-private double temperature;
-private List<String> history;
-public Patient(String name, int age, String condition,
-  int heartRate, String bloodPressure, double temperature) {
-    this.name = name;
-    this.age = age;
-    this.condition = condition;
-    this.heartRate = heartRate;
-    this.bloodPressure = bloodPressure;
-    this.temperature = temperature;
-    this.priority = EmergencyPriority.getPriority(condition);
-    this.history = new ArrayList<>();
-}
-public String getName() { return name; }
-public String getCondition() { return condition; }
-public String getPriority() { return priority; }
-public void addHistory(String record) {
-    history.add(record);
-}
-public void display() {
-    System.out.println("\nPatient: " + name);
-    System.out.println("Age: " + age);
-    System.out.println("Condition: " + condition);
-    System.out.println("Priority: " + priority);
-    System.out.println("Vitals: HR=" + heartRate +
-            ", BP=" + bloodPressure +
-            ", Temp=" + temperature);
-    System.out.println("History: " + history);
-}
-@Override
-public void run() {
-    try {
-        Thread.sleep(500);
-        HospitalSystem.addPatient(this);
-        System.out.println(name + " arrived and added to queue.");
-    } catch (InterruptedException e) {
-        System.out.println("Thread error: " + e.getMessage());
-}
-}
+public class Patient {
+ private String name;
+ private String cond;
+ private String priority;
+    public Patient(String name, String cond, String priority) {
+        this.name = name;
+        this.cond = cond;
+        this.priority = priority;
+    }
+  public String getName() {
+        return name;
+    }
+  public String getCond() {
+        return cond;
+    }
+ public String getPriority() {
+        return priority;
+    }
 }
